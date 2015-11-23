840c4bb104200c47ecb11b5b313841fb75ecfe5a
Garth Williams <garth@wi.llia.ms>
2015-11-23 09:39:42 +0100

    Moved some actions to an experimental set

---
+++ src/experimental/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  resetState: require('../factories/resetState.js'),
+  setWindowTitle: require('../factories/setWindowTitle.js'),
+  timeout: require('../factories/timeout.js'),
+  timer: require('../factories/timer.js'),
+  validate: require('../factories/validation.js')
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 module.exports = {
   copyInputToState: require('./factories/copyInputToState.js'),
   copyStateToOutput: require('./factories/copyStateToOutput.js'),
-  resetState: require('./factories/resetState.js'),
   set: require('./factories/set.js'),
-  setWindowTitle: require('./factories/setWindowTitle.js'),
-  timeout: require('./factories/timeout.js'),
-  timer: require('./factories/timer.js'),
   toggle: require('./factories/toggle.js'),
   unset: require('./factories/unset.js'),
-  validate: require('./factories/validation.js'),
   when: require('./factories/when.js')
 };
