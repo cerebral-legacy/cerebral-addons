454d6686db1f4ee6c5aa0e2b5b04b02533cea41c
Garth Williams <garth@wi.llia.ms>
2016-01-04 09:47:39 +0100

    Remove auto-generated items from repo.
    This closes #6

--- experimental/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  ajax: require('./ajax.js'),
-  resetState: require('./resetState.js'),
-  resetStateExcept: require('./resetStateExcept.js'),
-  timeout: require('./timeout.js'),
-  timer: require('./timer.js'),
-  validate: require('./validation.js')
-};
--- lib/README.md
+++
@@ -1,3 +0,0 @@
-# Don't make changes here
-
-All content in this folder is auto-generated. To make changes, please look at the /src folder.
