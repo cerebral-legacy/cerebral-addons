30dba90e7295a9be720783e4bc3a53438e4df5e1
Garth Williams <garth@wi.llia.ms>
2016-01-26 00:27:29 +0100

    Added state tests, two currently failing

--- src/state/findWhere.js
+++ src/state/findWhere.js
@@ -1,9 +1,9 @@
-import setCompiler from 'cerebral-url-scheme-compiler/set'
+import getCompiler from 'cerebral-url-scheme-compiler/get'
 import toDisplayName from '../helpers/toDisplayName'
 
 export default function (path, where) {
   // set compiler is more suited due to the additional param
-  const getter = setCompiler(path, 'merge')
+  const getter = getCompiler(path, 'findWhere')
   const findWhere = args => getter(args, where)
 
   findWhere.displayName = `findWhere(${toDisplayName(path, getter)}, ${JSON.stringify(where)})`
---
+++ test/state.js
@@ -0,0 +1,83 @@
+/*global beforeEach,afterEach,describe,it*/
+import './helpers/polyfills'
+import { expect } from 'chai'
+import controller from './helpers/controller'
+import copy from '../src/factories/copy'
+import findWhere from '../src/state/findWhere'
+import merge from '../src/state/merge'
+import pop from '../src/state/pop'
+import push from '../src/state/push'
+import shift from '../src/state/shift'
+import unshift from '../src/state/unshift'
+
+controller.signalsSync({
+  findWhere: [copy(findWhere('array', { value: 'c' }), 'output')],
+  merge: [copy('input:/value', merge('output'))],
+  pop: [copy(pop('array'), 'output')],
+  push: [copy('input:/value', push('array'))],
+  shift: [copy(shift('array'), 'output')],
+  unshift: [copy('input:/value', unshift('array'))]
+})
+const signals = controller.getSignals()
+
+let tree
+
+describe('state', function () {
+  afterEach(function () {
+    controller.reset()
+  })
+
+  beforeEach(function () {
+    tree = controller.model.tree
+    tree.set({
+      array: [{ value: 'a' }, { value: 'b' }, { value: 'c', other: 2 }, { value: 'd' }],
+      output: null
+    })
+    tree.commit()
+  })
+
+  describe('findWhere', function () {
+    it('finds the item', function () {
+      signals.findWhere()
+      expect(tree.get(['output'])).to.eql({ value: 'c', other: 2 })
+    })
+  })
+
+  describe('merge', function () {
+    it('merges state', function () {
+      tree.set('output', { before: 1 })
+      tree.commit()
+      signals.merge({ value: { merged: 'yes' } })
+      expect(tree.get(['output'])).to.eql({ before: 1, merged: 'yes' })
+    })
+  })
+
+  describe('pop', function () {
+    it('pops state', function () {
+      signals.pop()
+      expect(tree.get(['output'])).to.eql({ value: 'd' })
+    })
+  })
+
+  describe('push', function () {
+    it('push to state', function () {
+      signals.push({ value: { new: 'value' } })
+      const array = tree.get(['array'])
+      expect(array[array.length - 1]).to.eql({ new: 'value' })
+    })
+  })
+
+  describe('shift', function () {
+    it('shifts state', function () {
+      signals.shift()
+      expect(tree.get(['output'])).to.eql({ value: 'a' })
+    })
+  })
+
+  describe('unshift', function () {
+    it('unshift to state', function () {
+      signals.unshift({ value: { new: 'value' } })
+      expect(tree.get(['array'])[0]).to.eql({ new: 'value' })
+    })
+  })
+})
