dc686708258ffd01b63119ab78f0b8328da77522
Garth Williams <garth@wi.llia.ms>
2016-01-25 12:21:12 +0100

    Readded an index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  and: require('./and'),
+  copy: require('./copy'),
+  debounce: require('./debounce'),
+  not: require('./not'),
+  or: require('./or'),
+  set: require('./set'),
+  toggle: require('./toggle'),
+  unset: require('./unset'),
+  when: require('./when')
+}
