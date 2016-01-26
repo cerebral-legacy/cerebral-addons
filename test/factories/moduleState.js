11ed8c6ccd1f0fd233f7f77c9dfd8a93e9527952
Garth Williams <garth@wi.llia.ms>
2016-01-26 22:15:44 +0100

    Added some additional module state tests and updated url scheme compiler

---
+++ test/factories/moduleState.js
@@ -0,0 +1,41 @@
+/*global beforeEach,afterEach,describe,it*/
+import { expect } from 'chai'
+import controller from '../helpers/controller'
+import copy from '../../src/factories/copy'
+
+controller.modules({
+  mod: module => {
+    module.signalsSync({
+      copyAll: [ copy('state://./', 'output') ],
+      replaceAll: [ copy('input:/', 'state://./') ]
+    })
+  }
+})
+const signals = controller.getSignals()
+
+let tree
+
+describe('module state', function () {
+  afterEach(function () {
+    controller.reset()
+  })
+
+  beforeEach(function () {
+    tree = controller.model.tree
+    tree.set({
+      mod: { val: 'from module' },
+      output: null
+    })
+    tree.commit()
+  })
+
+  it('can be copied', function () {
+    signals.mod.copyAll()
+    expect(tree.get(['output'])).to.eql({ val: 'from module' })
+  })
+
+  it('can be replaced', function () {
+    signals.mod.replaceAll({ newVal: 'from input' })
+    expect(tree.get(['mod'])).to.eql({ newVal: 'from input' })
+  })
+})
