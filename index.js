a6b4b215d5d1ba7e8369e28b4c8b3eb56f629547
Garth Williams <garth@wi.llia.ms>
2016-01-27 09:01:18 +0100

    Export the state operators

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ module.exports = {
   and: require('./and'),
   copy: require('./copy'),
   debounce: require('./debounce'),
+  findWhere: require('./findWhere'),
+  merge: require('./merge'),
   not: require('./not'),
   or: require('./or'),
+  pop: require('./pop'),
+  push: require('./push'),
   set: require('./set'),
+  shift: require('./shift'),
   toggle: require('./toggle'),
   unset: require('./unset'),
+  unshift: require('./unshift'),
   when: require('./when')
 }
